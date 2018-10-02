befc775fdbcff54a0f0b36483467c6951790d55a
Daniel Schoch <d.schoch90@gmail.com>
2018-10-02 02:01:58 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -43,10 +43,10 @@ ORDER BY SUM(pledges.amount) ASC"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT pledges.amount, projects.category
-FROM pledges
-JOIN projects
-ON pledges.project_id = projects.id
+"SELECT projects.category, pledges.amount
+FROM projects
+JOIN pledges
+ON projects.id = pledges.project_id
 WHERE projects.category = 'music'"
 end
 
