d2276c1b101b9d3e88f79495de6d1c038cd397f1
Daniel Schoch <d.schoch90@gmail.com>
2018-10-02 01:09:48 +0000

    automatically backed up by learn

--- lib/create.sql
+++ lib/create.sql
@@ -5,9 +5,14 @@ CREATE TABLE users (
 );
 
 CREATE TABLE projects (
-  
+  id INTEGER PRIMARY KEY
+  title
+  category
+  funding_goal
+  start_date
+  end_date
 );
 
 CREATE TABLE pledges (
-  
+
 );
